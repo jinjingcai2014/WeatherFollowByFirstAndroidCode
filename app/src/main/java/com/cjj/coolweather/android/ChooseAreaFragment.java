290cfef89ff2553a4ebaf31cc05a832d0150083a
caijingjin <caijj1@chanjet.com>
2018-05-26 23:54:35 +0800

    增加省市区

--- app/src/main/java/com/cjj/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/cjj/coolweather/android/ChooseAreaFragment.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.cjj.coolweather.android.db.City;
 import com.cjj.coolweather.android.db.Country;
 import com.cjj.coolweather.android.db.Province;
+import com.cjj.coolweather.android.gson.AQI;
 import com.cjj.coolweather.android.util.HttpUtil;
 import com.cjj.coolweather.android.util.Utility;
 
@@ -95,10 +96,20 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if(currentLevel == LEVEL_COUNTRY){
                     String weatherId = countryList.get(position).getWeatherId();
+                    if(getActivity()instanceof  MainActivity){
+
+
                     Intent intent = new Intent(getActivity(),WeatherActivity.class);
                     intent.putExtra("weather_id",weatherId);
                     startActivity(intent);
                     getActivity().finish();
+                    }else if(getActivity() instanceof  WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
 
             }
--- app/src/main/java/com/cjj/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/cjj/coolweather/android/WeatherActivity.java
@@ -5,12 +5,16 @@ import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.provider.FontsContract;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -34,6 +38,7 @@ import okhttp3.Response;
 public class WeatherActivity extends AppCompatActivity {
 
     public final String key = "4c9d7506085e44f08a3e4facc2531a82 ";
+
     private ScrollView weatherLayout;
 
     private TextView titleCity;
@@ -57,6 +62,12 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView sportText;
 
     private ImageView bingPicImg;
+
+    public SwipeRefreshLayout swipeRefreshLayout;
+
+    public DrawerLayout drawerLayout;
+
+    private Button navButton;
 
 
     @Override
@@ -82,19 +93,36 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView)findViewById(R.id.car_wash_text);
         sportText = (TextView)findViewById(R.id.sport_text);
         bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navButton = (Button)findViewById(R.id.nav_button);
 
-
+        navButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = preferences.getString("weather",null);
+        final String weatherId;
         if(weatherString != null){
             Weather weather = Utility.HandleWeatherResponse(weatherString);
+            weatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         }else{
             //本地缓存没有话，去服务器上拉取
-            String weatherId= getIntent().getStringExtra("weather_id");
+            weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
         }
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(weatherId);
+            }
+        });
         String bingPic = preferences.getString("bing_pic",null);
         if(bingPic != null){
             Glide.with(this).load(bingPic).into(bingPicImg);
@@ -137,7 +165,7 @@ public class WeatherActivity extends AppCompatActivity {
     /// </summary>
     /// <param name="XXX"></param>
     /// <returns></returns>
-    private void requestWeather(String weatherId)
+    public  void requestWeather(String weatherId)
     {
         String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId +"&key="+key;
 
@@ -150,6 +178,7 @@ public class WeatherActivity extends AppCompatActivity {
                         Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                     }
                 });
+                swipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
@@ -169,6 +198,7 @@ public class WeatherActivity extends AppCompatActivity {
                         }
                     }
                 });
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
         loadBingPic();
